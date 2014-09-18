a507ff81600a004d8bba44897d3af7f43c049092
Nickrhys <nickrhys@hotmail.com>
2014-09-18 17:23:21 +0100

    temerature control tested and passed. gen functionality in progress

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -14,5 +14,43 @@ describe("Thermostat", function() {
     it("should have a power saving mode on initialization", function() {
       expect(thermostat.powerSavingMode).toBe(true);
     });
+
+    describe("temperature control", function() {
+
+      it("should allow you to increase temperature", function() {
+        thermostat.temperature = 20;
+        thermostat.increase();
+        expect(thermostat.temperature).toEqual(21);
+      });
+
+      it("should allow you to decrease temperature", function() {
+        thermostat.temperature = 20;
+        thermostat.decrease();
+        expect(thermostat.temperature).toEqual(19);
+      });
+
+      describe("general functionality", function() {
+
+        it("should not allow the temperature to go below 10", function(){
+          thermostat.temperature = 10;
+          thermostat.decrease();
+          expect(thermostat.temperature).toEqual(10)
+        });
+
+        it("should not allow the temperature to go above 25 in Power Saving Mode", function(){
+          this.powerSavingMode= true;
+          thermostat.temperature = 25;
+          thermostat.increase();
+          expect(thermostat.temperature).toEqual(25);
+        });
+
+        it("should not allow the temperature to go above 32 when Power Saving Mode is off", function(){
+          // this.powerSavingMode = false;
+          thermostat.temperature = 32;
+          thermostat.increase();
+          expect(thermostat.temperature).toEqual(32);
+        });
+      });
+    });
   });
 });
