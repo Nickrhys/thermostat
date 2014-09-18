488935518fd93af14271064e315c17c453e1ffa9
Nickrhys <nickrhys@hotmail.com>
2014-09-18 16:11:56 +0100

    inititialized thermostat and tested

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -5,6 +5,14 @@ describe("Thermostat", function() {
     thermostat = new Thermostat();
   });
 
+  describe("initialization of thermostat", function() {
+   
+    it("should have an initialization temperature of 20 degrees", function() {
+      expect(thermostat.temperature).toEqual(20);
+    }); 
 
-
+    it("should have a power saving mode on initialization", function() {
+      expect(thermostat.powerSavingMode).toBe(true);
+    });
+  });
 });
